0f39033906bc0004ea15e6fe06bf5a61b2feed3f
barbara73 <barbara.jesacher@gmail.com>
2022-06-19 13:29:58 +0200

    Functional tes now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -6,15 +8,38 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-
     def tearDown(self):
         self.browser.quit()
-
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Invited to enter a to-do item 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        
+        # Typed buy things into text box
+        inputbox.send_keys('Buy things')
+
+        # When hitting enter, the page updates and page lists
+        # "1: Buy things" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_anme('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy things' for row in rows)
+        )
+
+
         self.fail('Finish the test!')
 
 
--- superlists/urls.py
+++ superlists/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import path 
 from lists import views
 
 urlpatterns = [
-    url('$', views.home_page, name='home'),
+    path('', views.home_page, name='home'),
 ]
